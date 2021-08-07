8ebda161460db851f8b212d147862253a44cba33
Burhan ul haq <burhanhaq97@gmail.com>
2021-08-07 22:59:37 +0500

    modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "../src/landingPage/App";
+// import App from "../src/landingPage/App";
+// import App from "../src/captcha/App";
+import App from '../src/BlogSection/App'
 
 ReactDom.render(
   <>
